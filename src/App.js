c0896b2c07f34a5b767256a00f6ea76e4fc7c8e0
Bikram <you@example.com>
2018-10-26 10:50:56 +0530

    dynamic position feature added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+//Relative imports
 import Rect from './Rect';
 
 const data = [
-    [30,"red","c001"],
-    [45,"green","c002"],
-    [60,"green","c003"],
-    [14,"green","c004"]
-]
-class App extends Component {
-   render() {
-   return (
-      <div className='App'>
-      <div className='App-header'>
-      </div>
-      <div>
-          {
-              data.map(data=>{
+                [30,"red","c001",30,10],
+                [45,"green","c002",1050,10],
+                [60,"green","c003",650,470],
+                [14,"green","c004",160,470]
+            ]
 
-                  return <Rect key={data[0]} data={[data]}/>
-              })
-          }
-      </div>
-      </div>
-   )
-   }
+const svgcontainerpostion = {
+                                position: "relative"
+                            }
+
+class App extends Component {
+
+   render() {
+            return (
+               <div className='App'>
+                    <div style={svgcontainerpostion}>
+                        {
+                            data.map(data=>{
+                                return <Rect key={data[0]} data={[data]}/>
+                            })
+                        }
+                    </div>
+               </div>
+            )
+        }        
 }
 export default App
--- src/Rect.js
+++ src/Rect.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { select } from 'd3-selection'
-import PropTypes from 'prop-types';
+import { select } from 'd3-selection';
 class Rect extends Component {
    constructor(props){
       super(props)
@@ -14,59 +13,56 @@ class Rect extends Component {
    }
    createRect() {
     var data= this.props.data;
-    // // const {data}=this.props;
-    // console.log((this.props.data));
-    // console.log(data);
-      const node = this.node
-     const canvas = select(node)
-            .data(data)
-            .append("svg")
-            .attr("width",150)
-            .attr("height",150)
+    var width = 150;
+    var height = 130;
 
+    const node = this.node 
 
-            canvas
-            .append("rect")
-            .style("fill",(d)=>d[1])
-            .attr("x",15)
-            .attr("y",10)
-            .attr("width",150)
-            .attr("height",150)
+    const canvas = select(node) //Binding the DOM SVG Element with d3
+                  .data(data)   //Binding the {data}
+                  .append("g")  //To keep multiple parts in one group
+                  .attr("width","auto")
+                  .attr("height","auto")
+    //Rectangle 1   
+    canvas
+      .append("rect")  //Adding Rectangle which displays the {color} and {build percentage(%)}
+      .style("fill",(d)=>d[1])
+      .attr("x",(d)=>d[3]) //Getting X coordinates from Json
+      .attr("y",(d)=>d[4]) //Getting Y coordinates from Json
+      .attr("width",width)
+      .attr("height",height)
       
-          canvas.selectAll("text")
-          .data(data)
-          .enter()
-          .append("text")
-          .attr("x",15+50)
-          .attr("y",20+45)
-          .attr("font-size","20px")
-          .text((d)=>d[0]+"%")
+
+    canvas.selectAll("text") // Adding a text to display the build percentage in Rectangle 1
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("x",(d)=>d[3]+(width/2)-15)
+      .attr("y",(d)=>d[4]+(height/2)+10)
+      .attr("font-size","20px")
+      .text((d)=>d[0]+"%")
+
+  //Rectangle 2
+    canvas
+      .append("rect") //Adding  Rectangle which displays the {Chamber number}
+      .style("fill","yellow")
+      .attr("x",(d)=>d[3])
+      .attr("y",(d)=>d[4]+height)
+      .attr("width",150)
+      .attr("height",50)
       
-          canvas
-            .append("rect")
-            .style("fill","yellow")
-            .attr("x",15)
-            .attr("y",110)
-            .attr("width",150)
-            .attr("height",50)
-      
-          canvas
-          .append("text")
-          .attr("x",15+50)
-          .attr("y",120+15)
-          .text((d)=>d[2])
-          .attr("font-size","15px")
+    canvas
+      .append("text") // Adding  text to display the {chamber Number}  in Rectangle 2
+      .attr("x",(d)=>d[3]+55)
+      .attr("y",(d)=>d[4]+height+30)
+      .text((d)=>d[2])
+      .attr("font-size","20px")
 
    }
-render() {
-      return <div ref={node => this.node = node}>
-      </div>
+  render() {
+      //svg-position class is defined in index.html to set the svg position to absolute 
+      return <svg className="svg-position" ref={node => this.node = node} width="100vw" height="100vh"> </svg> 
    }
 }
-Rect.defaultProps ={
-  data:[]
-}
-Rect.propTypes ={
-  data:PropTypes.array
-}
+
 export default Rect;
